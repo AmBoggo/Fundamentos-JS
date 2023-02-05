11c472b5957b3689bf5f020879effc1cfa8f915d
Alisson <alisson.mb@gmail.com>
2023-02-05 19:36:01 -0300

    variáveis e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+//tipos de dados
+//boolean
+
+//conversões implícitas
+const numero = 456;
+const string = "456";
+
+
+//conversões explícitas
+//Number() converte texto para número
+//String() converte números par texto
+console.log(numero == string);
+console.log(numero + string);
+console.log(numero + Number(string));
--- tiposBoolean.js
+++ tiposBoolean.js
@@ -2,3 +2,26 @@ const primeiroNumero = 5;
 const segundoNumero = 5;
 
 console.log(primeiroNumero === segundoNumero)
+
+//boolean 2
+const usuarioLogado = true;
+const contaPaga = false;
+//pode ser atribuido diretamente a condição de verdadeiro ou falso.
+
+//truthy e falsy - "tipo verdadeiro ou falso" 
+//se comportam como true e false
+
+// 0 => false
+// "" => false
+// 1 => true
+
+console.log(0 == false)
+
+//null (vazio/nada)
+
+// uma variável let sem valor ganha undefined 
+
+// tipo da variável undefined é undefined
+// tipo da variável null é object (erro origem js não será ajustado)
+
+
