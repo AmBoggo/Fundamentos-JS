713a409e475004bdebea6c317884faa700a8d869
Alisson <alisson.mb@gmail.com>
2023-02-05 18:43:38 -0300

    exemplos de tipos

---
+++ tiposStrings.js
@@ -0,0 +1,16 @@
+const texto1 = "Ola, mundo!";
+const texto2 = 'Ola, mundo!';
+const senha = "senhaSuperSegura456@";
+const stringDeNumeros = "34567";
+
+const citacao = 'O Leao disse "oi"';
+
+//template string ou template literal
+
+const template = `Utilizando template`;
+
+// concatenação (+)
+const texto = "meu nome é: "
+const nome = "Alisson"
+
+console.log(texto + nome)
